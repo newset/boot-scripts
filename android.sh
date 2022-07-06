bd4978e67a8b65c2d92599469db9d4b1cd0a4051
罗杰 <luojieyy@gmail.com>
2022-07-06 11:50:49 +0800

    Create android.sh

---
+++ android.sh
@@ -0,0 +1,19 @@
+# install android studio
+brew install java openjdk
+brew install --cask android-studio
+
+echo 'export ANDROID_HOME=$HOME/Library/Android/sdk' >> ~/.zprofile
+export ANDROID_HOME=$HOME/Library/Android/sdk
+
+# install
+curl https://dl.google.com/android/repository/commandlinetools-mac-8512546_latest.zip  >> $ANDROID_HOME/cmdline-tools.zip
+cd $ANDROID_HOME
+unzip cmdline-tools.zip -d .
+rm cmdline-tools.zip
+
+# proxy
+# mirrors.neusoft.edu.cn:80
+
+# install ndk
+yes | ${sdk}/cmdline-tools/bin/sdkmanager --licenses
+   
