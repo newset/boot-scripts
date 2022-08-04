8883fb3ef6260dc6dfa12d85765cb9c980e09875
罗杰 <luojieyy@gmail.com>
2022-08-04 16:20:12 +0800

    move install flutter

--- install.sh
+++ install.sh
@@ -79,23 +79,6 @@ git clone https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git master
 echo "安装rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-cd $HOME
-echo "downloading flutter sdk from mirror flutter-io.cn"
-if [ `arch` != 'arm64' ]
-then
-    echo "get intel chip flutter"
-    curl https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos_3.0.5-stable.zip > flutter.zip
-else
-    echo "get arm chip flutter"
-    curl https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.0.5-stable.zip > flutter.zip
-fi
-
-unzip flutter.zip
-
-echo "set flutter envs"
-export PUB_HOSTED_URL=https://pub.flutter-io.cn
-export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
-
 echo "install jetbrains mono"
 
 curl -L https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip | bsdtar -xvf-
