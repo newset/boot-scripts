44a2330c739302ffc6537dba7545c2abac397fdc
罗杰 <luojieyy@gmail.com>
2022-08-04 16:08:58 +0800

    update flutter install

--- install.sh
+++ install.sh
@@ -81,7 +81,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 cd $HOME
 echo "downloading flutter sdk from mirror flutter-io.cn"
-if [ `arch` == 'i386' ]
+if [ `arch` != 'arm64' ]
 then
     echo "get intel chip flutter"
     curl https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos_3.0.5-stable.zip > flutter.zip
@@ -89,6 +89,8 @@ else
     echo "get arm chip flutter"
     curl https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.0.5-stable.zip > flutter.zip
 fi
+
+unzip flutter.zip
 
 echo "set flutter envs"
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
