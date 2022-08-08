537531ffc90ef37f6a47841500b9fbf6a30256a4
罗杰 <luojieyy@gmail.com>
2022-08-08 17:54:04 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,12 +1,7 @@
+# install vscode
+echo "install vscode"
+brew install --cask visual-studio-code
+
 # install nvm and node
 echo "install nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install 16
-nvm use 16
-
-npm config set registry http://r.cnpmjs.org --global
-npm config set disturl https://npm.taobao.org/mirrors/node --global
-
-npm i yarn -g
-yarn config set registry http://r.cnpmjs.org --global
-yarn config set disturl https://npm.taobao.org/mirrors/node --global
