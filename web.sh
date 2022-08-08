772261396d88a0f4a00748d4812d67f9a32ff62e
罗杰 <luojieyy@gmail.com>
2022-08-08 18:59:03 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -5,3 +5,15 @@ brew install --cask visual-studio-code
 # install nvm and node
 echo "install nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install 14.19.0
+
+npm config set registry http://r.cnpmjs.org --global
+npm config set disturl https://npm.taobao.org/mirrors/node --global
+
+npm i yarn -g
+yarn config set registry http://r.cnpmjs.org --global
+yarn config set disturl https://npm.taobao.org/mirrors/node --global
