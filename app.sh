be545fc26fc7e08f9a811ec75cd478575a904b8b
罗杰 <luojieyy@gmail.com>
2022-08-08 17:31:21 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,12 +2,6 @@
 
 brew install tmux unar python3
 
-# install nvm and node
-echo "install nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install 16
-nvm use 16
-
 echo "install docker golang docker-compose iterm2"
 brew install --cask --appdir=/Applications docker
 brew install golang docker-compose iterm2
@@ -18,10 +12,3 @@ brew install --cask visual-studio-code
 
 echo "install raycast"
 brew install --cask raycast 
-
-npm config set registry http://r.cnpmjs.org --global
-npm config set disturl https://npm.taobao.org/mirrors/node --global
-
-npm i yarn -g
-yarn config set registry http://r.cnpmjs.org --global
-yarn config set disturl https://npm.taobao.org/mirrors/node --global
