1cf506faedcc9b06e93adfa804238d7b5bc82663
罗杰 <luojieyy@gmail.com>
2022-08-08 17:31:35 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,13 @@
-
 # install nvm and node
+echo "install nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install --lts
-nvm use --lts
+nvm install 16
+nvm use 16
+
+npm config set registry http://r.cnpmjs.org --global
+npm config set disturl https://npm.taobao.org/mirrors/node --global
+
+npm i yarn -g
+yarn config set registry http://r.cnpmjs.org --global
+yarn config set disturl https://npm.taobao.org/mirrors/node --global
+
